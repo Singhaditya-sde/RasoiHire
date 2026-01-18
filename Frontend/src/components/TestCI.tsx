5cc42bfd8a81a74845476ed1606d23efd4c6124b
Aditya Kumar <singhaditya.cs@gmail.com>
2026-01-19 01:16:26 +0530

    Add test component to verify CI/CD pipeline

---
+++ Frontend/src/components/TestCI.tsx
@@ -0,0 +1,12 @@
+const TestCI = () => {
+  return (
+    <div className="flex justify-between ">
+      <div className="p-2 bg-amber-500 rounded-2xl items-center">
+      <h2>CI/CD Test Component ✅</h2>
+      <p>If you see this, CI pipeline worked.</p>
+    </div>
+    </div>
+  );
+};
+
+export default TestCI;
