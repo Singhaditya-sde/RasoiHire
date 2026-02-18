159d4ea41d1a4a55a84362d4cd380087d05b58bd
Aditya Kumar <singhaditya.cs@gmail.com>
2026-02-18 22:48:42 +0530

    Add cmessage display to TestCI component

--- Frontend/src/components/TestCI.tsx
+++ Frontend/src/components/TestCI.tsx
@@ -4,6 +4,7 @@ const TestCI = () => {
       <div className="p-2 bg-amber-500 rounded-2xl items-center">
       <h2>CI/CD Test Component ✅</h2>
       <p>If you see this, CI pipeline worked.</p>
+      <p>code commit </p>
     </div>
     </div>
   );
